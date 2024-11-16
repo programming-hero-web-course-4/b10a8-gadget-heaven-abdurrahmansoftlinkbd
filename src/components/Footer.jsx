b4f2df399c4825c083d0777cdfc094a1ae9c3263
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-16 23:08:58 +0600

    footer modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,39 @@
 const Footer = () => {
   return (
-    <footer className="footer bg-neutral text-neutral-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </footer>
+    <>
+      <div className="hero bg-gray-100 mt-24 font-sora">
+        <div className="hero-content pt-24 text-center">
+          <div>
+            <h1 className="text-4xl font-bold ">Gadget Heaven</h1>
+            <p className="mt-3 mb-7 text-gray3 font-medium">
+              Leading the way in cutting-edge technology and innovation.
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className="container mx-auto" />
+      <footer className="footer bg-gray-100 container mx-auto justify-items-center font-sora p-10">
+        <nav>
+          <h6 className="footer-title opacity-100">Services</h6>
+          <a className="link text-gray3 link-hover">Product Support</a>
+          <a className="link text-gray3 link-hover">Order Tracking</a>
+          <a className="link text-gray3 link-hover">Shipping & Delivery</a>
+          <a className="link text-gray3 link-hover">Returns</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title opacity-100">Company</h6>
+          <a className="link text-gray3 link-hover">About us</a>
+          <a className="link text-gray3 link-hover">Careers</a>
+          <a className="link text-gray3 link-hover">Contact</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title opacity-100">Legal</h6>
+          <a className="link text-gray3 link-hover">Terms of Service</a>
+          <a className="link text-gray3 link-hover">Privacy Policy</a>
+          <a className="link text-gray3 link-hover">Cookie Policy</a>
+        </nav>
+      </footer>
+    </>
   );
 };
 
