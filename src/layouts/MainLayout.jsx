03de7c5ae683cb830577f2495702a4ec0d3e274c
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-16 13:18:49 +0600

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      {/* navbar */}
+      <Navbar></Navbar>
+      {/* dynamic parts */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/GadgetDetails.jsx
@@ -0,0 +1,9 @@
+const GadgetDetails = () => {
+  return (
+    <div>
+      <h2>GadgetDetails</h2>
+    </div>
+  );
+};
+
+export default GadgetDetails;
