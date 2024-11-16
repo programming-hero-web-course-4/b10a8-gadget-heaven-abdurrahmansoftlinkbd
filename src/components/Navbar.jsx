392a0fcd1d12cca962722d059b46c59152eaaa15
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-16 14:06:59 +0600

    navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,23 +27,15 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            <NavLink to="/" className="font-medium">
+              Home
+            </NavLink>
+            <NavLink to="/statistics" className="font-medium">
+              Statistics
+            </NavLink>
+            <NavLink to="/dashboard" className="font-medium">
+              Dashboard
+            </NavLink>
           </ul>
         </div>
         <Link to="/" className="text-xl font-bold">
