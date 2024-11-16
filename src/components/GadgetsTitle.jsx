75acf3d1634f32bc3d27c77d66a9aa1383301e82
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-11-16 16:03:31 +0600

    added gadgetsTitle

---
+++ src/components/GadgetsTitle.jsx
@@ -0,0 +1,9 @@
+const GadgetsTitle = () => {
+  return (
+    <h2 className="font-sora font-bold text-4xl text-center">
+      Explore Cutting-Edge Gadgets
+    </h2>
+  );
+};
+
+export default GadgetsTitle;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import Banner from "../components/Banner";
+import GadgetsTitle from "../components/GadgetsTitle";
 
 const Home = () => {
   return (
-    // banner
-    <Banner></Banner>
+    <>
+      {/* banner */}
+      <Banner></Banner>
+      {/* gadgetsTitle */}
+      <GadgetsTitle></GadgetsTitle>
+    </>
   );
 };
 
